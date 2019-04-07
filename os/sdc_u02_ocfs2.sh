914fdd8234e90038d33e62b8f2f06eb9c43b361c
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-04-07 12:43:13 +0200

    Update sdc_u02_ocfs2.sh

--- os/sdc_u02_ocfs2.sh
+++ os/sdc_u02_ocfs2.sh
@@ -9,7 +9,7 @@ fi
 blkid /dev/sdc*
 if [ $? -ne 0 ]; then
   mkfs.ocfs2 -M local -T mail /dev/sdc
-  blkid /dev/sdc 2>&1>/dev/null && echo $(blkid /dev/sdc -o export | head -n1) /u02 ocfs2 defaults 0 0 >> /etc/fstab
+  blkid /dev/sdc 2>&1>/dev/null && echo $(blkid /dev/sdc -o export | grep '^UUID=') /u02 ocfs2 defaults 0 0 >> /etc/fstab
   mkdir -p /u02
   mount /u02
 else
